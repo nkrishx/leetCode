1b7a22d0825f2a3fa4f19f6d167a5995df6c26f3
nkrishx <naveen.gowda.rm@gmail.com>
2022-01-27 22:33:28 +0000

    commit

--- 187-repeatedDNSSequence.py
+++ 187-repeatedDNSSequence.py
@@ -6,7 +6,7 @@ class Solution:
         if len(s) < 10:
             return []
         
-        for i in range(0,len(s)-10+1):
+        for i in range(0,len(s)+1-10):
             if s[i:i+10] in sequences:
                 result.add(s[i:i+10])
             sequences.add(s[i:i+10])
