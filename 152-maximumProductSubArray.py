247c2292a80558f592d83a85160943cb138d86e6
nkrishx <naveen.gowda.rm@gmail.com>
2022-02-10 17:56:58 +0000

    commit

--- 152-maximumProductSubArray.py
+++ 152-maximumProductSubArray.py
@@ -1,16 +1,36 @@
 class Solution:
     def maxProduct(self, nums: List[int]) -> int:
+        '''
+        at any given number we have choose from 3 choices
+        the current number, the previous max * current number or the previous min * current number
+        '''
         if len(nums) == 0:
             return 0
 
-        result = -sys.maxsize
+        max_so_far = nums[0]
+        min_so_far = nums[0]
+        result = max_so_far
 
-        for i in range(len(nums)):
-            accu = 1
-            for j in range(i, len(nums)):
-                accu *= nums[j]
-                result = max(result, accu)
+        for i in range(1, len(nums)):
+            curr = nums[i]
+            temp_max = max(curr, max_so_far * curr, min_so_far * curr)
+            min_so_far = min(curr, max_so_far * curr, min_so_far * curr)
+
+            max_so_far = temp_max
+
+            result = max(max_so_far, result)
 
         return result
 
-        
+        # if len(nums) == 0:
+        #     return 0
+
+        # result = -sys.maxsize
+
+        # for i in range(len(nums)):
+        #     accu = 1
+        #     for j in range(i, len(nums)):
+        #         accu *= nums[j]
+        #         result = max(result, accu)
+
+        # return result
